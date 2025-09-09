545fb8d806b069a234950badc273c1e80b2abf17
durgesh awashti <133563743+awasthi16@users.noreply.github.com>
2025-09-09 18:24:23 +0530

    Create java_question.java

---
+++ java_question.java
@@ -0,0 +1,47 @@
+Write a program to find the length of a string without using length() method.
+
+Check whether a given string is empty or not using isEmpty().
+
+Find the character at the middle index of a string using charAt().
+
+Print the Unicode value of each character in "Java" using codePointAt().
+
+Compare two strings "Hello" and "hello" using equals() and equalsIgnoreCase().
+
+Searching & Checking (6–10)
+
+Check if "programming" contains "gram" using contains().
+
+Check if "www.example.com" starts with "www" and ends with "com".
+
+Find the first and last index of 'a' in "Banana".
+
+Write a program that counts how many times "the" occurs in "the cat in the hat".
+
+Extract the domain name from "https://openai.com" using substring().
+
+
+
+Replace all 'a' with '@' in "Java" using replace().
+
+Replace all digits in "abc123xyz456" with * using replaceAll().
+
+Trim spaces from " Hello World " using trim().
+
+Convert "Hello World" to uppercase and lowercase.
+
+Reverse a string (without using StringBuilder) using toCharArray().
+
+
+
+Split "apple,banana,grapes" by comma and print each fruit.
+
+Join the words ["I","love","Java"] into a single string with " " (space) as delimiter.
+
+Count the number of words in a sentence entered by user using split(" ").
+
+
+
+Write a program to check if a string is a palindrome (same forward and backward).
+
+Repeat "Hi" 5 times using repeat() and print the result.
